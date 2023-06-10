20f96cdd51a743dbecdf31f3a1fa41db8fc2a3ac
Fernando Perry <fernandoperry1234@gmail.com>
2023-06-10 18:59:29 +0700

    couple of update and cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,14 @@
 use serde_derive::Deserialize;
-use toml;
 use std::env;
-use std::path;
 use std::fs;
 use std::io::Write;
+use std::path;
+use toml;
 
 #[derive(Deserialize)]
 pub struct RmlistConfig {
     pub media: Vec<String>,
-    pub other_flag: Vec<String>
+    pub other_flag: Vec<String>,
 }
 
 #[derive(Deserialize)]
@@ -16,18 +16,19 @@ pub struct Config {
     pub media_list_path: Vec<String>,
 }
 
-pub fn get_configuration() -> Result<Config, String>{
+pub fn get_configuration() -> Result<Config, String> {
     let mut path_to_conf: String = match env::var("HOME") {
         Ok(val) => val,
-        Err(err) => return Err(err.to_string())
+        Err(err) => return Err(err.to_string()),
     };
     path_to_conf.push_str("/.config/rmlist.toml");
     match path::Path::new(&path_to_conf).is_file() {
         false => {
-            let mut create_config: fs::File = fs::File::create(&path_to_conf).expect("Error encountered while creating file!");
+            let mut create_config: fs::File =
+                fs::File::create(&path_to_conf).expect("Error encountered while creating file!");
             match create_config.write_all(b"media_list_path = []") {
-                Ok(_) => {},
-                Err(err) => return Err(err.to_string())
+                Ok(_) => {}
+                Err(err) => return Err(err.to_string()),
             };
             println!("WARN : Created the config file.");
             return Err("WARN : Exiting..".to_string());
@@ -35,7 +36,7 @@ pub fn get_configuration() -> Result<Config, String>{
         true => {
             let contents: String = match fs::read_to_string(path_to_conf) {
                 Ok(val) => val,
-                Err(err) => return  Err(err.to_string())
+                Err(err) => return Err(err.to_string()),
             };
             let mut data: Config = match toml::from_str(&contents) {
                 Ok(d) => d,
@@ -45,9 +46,9 @@ pub fn get_configuration() -> Result<Config, String>{
             };
             for i in 0..data.media_list_path.len() {
                 let path_char: Vec<char> = data.media_list_path[i].chars().collect();
-                match path_char[path_char.len()-1]{
-                    '/' => {},
-                    _ => data.media_list_path[i].push_str("/")
+                match path_char[path_char.len() - 1] {
+                    '/' => {}
+                    _ => data.media_list_path[i].push_str("/"),
                 }
             }
             return Ok(data);
@@ -55,12 +56,12 @@ pub fn get_configuration() -> Result<Config, String>{
     }
 }
 
-pub fn get_rmlist_configuration(path_to_rmlist:String) -> Result<RmlistConfig, String>{
+pub fn get_rmlist_configuration(path_to_rmlist: String) -> Result<RmlistConfig, String> {
     match path::Path::new(&path_to_rmlist).is_file() {
         true => {
             let contents: String = match fs::read_to_string(path_to_rmlist) {
                 Ok(val) => val,
-                Err(err) => return  Err(err.to_string())
+                Err(err) => return Err(err.to_string()),
             };
             let data = match toml::from_str(&contents) {
                 Ok(d) => d,
@@ -70,16 +71,16 @@ pub fn get_rmlist_configuration(path_to_rmlist:String) -> Result<RmlistConfig, S
             };
             return Ok(data);
         }
-        _ => return Err("ERR : The specified path for rmlist file doesnt exist.".to_string())
+        _ => return Err("ERR : The specified path for rmlist file doesnt exist.".to_string()),
     }
 }
 
-pub fn create_rmlist(path_to_rmlist:&String) -> u8{
-    let mut create_config: fs::File = fs::File::create(&path_to_rmlist).expect("WARN : Error encountered while creating file!");
+pub fn create_rmlist(path_to_rmlist: &String) -> u8 {
+    let mut create_config: fs::File =
+        fs::File::create(&path_to_rmlist).expect("WARN : Error encountered while creating file!");
     match create_config.write_all(b"media = []\nother_flag = []") {
-        Ok(_) => {},
-        Err(_) => return 1
+        Ok(_) => {}
+        Err(_) => return 1,
     }
     return 0;
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod config;
-use config::{Config, get_configuration, RmlistConfig, get_rmlist_configuration, create_rmlist};
+use config::{create_rmlist, get_configuration, get_rmlist_configuration, Config, RmlistConfig};
 use std::env;
 use std::path;
 use std::process;
 
 enum Mode {
     Play,
-    Create
+    Create,
 }
 
 struct Arguments {
@@ -15,51 +15,66 @@ struct Arguments {
 }
 
 fn get_args() -> Result<Arguments, String> {
-    let user_args:Vec<String> = env::args().collect();
+    let user_args: Vec<String> = env::args().collect();
     if user_args.len() < 2 {
         return Err("ERR: Didnt get any argument.".to_string());
-    }
-    else if user_args.len() < 3 {
+    } else if user_args.len() < 3 {
         return Err("ERR : Didnt get any rmlist file name.".to_string());
     }
-    match &user_args[1][..]{
-        "play" => return Ok(Arguments {mode:Mode::Play, mlist:user_args[2].clone()}),
-        "create" => return Ok(Arguments {mode:Mode::Create, mlist:user_args[2].clone()}),
-        _ => return Err("ERR : Invalid argument.".to_string())
+    match &user_args[1][..] {
+        "play" => {
+            return Ok(Arguments {
+                mode: Mode::Play,
+                mlist: user_args[2].clone(),
+            })
+        }
+        "create" => {
+            return Ok(Arguments {
+                mode: Mode::Create,
+                mlist: user_args[2].clone(),
+            })
+        }
+        _ => return Err("ERR : Invalid argument.".to_string()),
     }
 }
 
-fn combine(mlist:&String, path_to_find:&String) -> String{
-    let mut p_mlist:String = mlist.clone();
+fn combine(mlist: &String, path_to_find: &String) -> Result<String, String> {
+    let mut p_mlist: String = mlist.clone();
     if !p_mlist.contains(".rmlist") {
         p_mlist.push_str(".rmlist");
     }
-    let full_path:String = format!("{}{}", path_to_find, p_mlist);
+    let full_path: String = format!("{}{}", path_to_find, p_mlist);
     match path::Path::new(&full_path).is_file() {
-        false => process::exit(1),
+        false => return Err("ERR : the specified file doesnt exist.".to_string()),
         _ => {}
     };
-    return full_path;
+    return Ok(full_path);
 }
 
-fn play(full_path:&String) {
+fn play(full_path: &String) {
     let rmlist_content: RmlistConfig = match get_rmlist_configuration(full_path.to_string()) {
         Ok(val) => val,
         Err(err) => {
             println!("{err}");
-            process::exit(1);
+            return;
         }
     };
     for media in &rmlist_content.media {
-        match path::Path::new(&media).is_file() {
-            false => println!("WARN : `{media}` doesnt exist. Skipping..."),
-            _ => {}
+        if !&media.contains("https://") {
+            match path::Path::new(&media).is_file() {
+                false => println!("WARN : `{media}` doesnt exist. Skipping..."),
+                _ => {}
+            }
         }
     }
-    spawn_process("/usr/bin/mpv".to_string(), rmlist_content.media, rmlist_content.other_flag);
+    spawn_process(
+        "/usr/bin/mpv".to_string(),
+        rmlist_content.media,
+        rmlist_content.other_flag,
+    );
 }
 
-fn spawn_process(program:String, file:Vec<String>,flag:Vec<String>) {
+fn spawn_process(program: String, file: Vec<String>, flag: Vec<String>) {
     process::Command::new(program)
         .args(flag)
         .args(file)
@@ -85,16 +100,21 @@ fn main() {
     match prog_args.mode {
         Mode::Play => {
             let mlist_path_char: Vec<char> = prog_args.mlist.chars().collect();
-            if mlist_path_char[0] == '/' || mlist_path_char[0] == '.'{
+            if mlist_path_char[0] == '/' || mlist_path_char[0] == '.' {
                 play(&prog_args.mlist);
             } else {
-                for i in 0..user_conf.media_list_path.len() {
-                    play(&combine(&prog_args.mlist, &user_conf.media_list_path[i]));
+                for path in user_conf.media_list_path {
+                    match combine(&prog_args.mlist, &path) {
+                        Ok(val) => {
+                            play(&val);
+                        }
+                        Err(err) => println!("{err}"),
+                    };
                 }
             }
-        },
+        }
         Mode::Create => {
-            let mut p_mlist:String = prog_args.mlist.clone();
+            let mut p_mlist: String = prog_args.mlist.clone();
             if !p_mlist.contains(".rmlist") {
                 p_mlist.push_str(".rmlist");
             }
